faca2cd16401f4dfb2134c6e5866ec379450b413
o <o@DESKTOP-C2RKL8H>
2019-10-31 13:39:45 +0900

    create Project

---
+++ src/test/java/com/fileSearch/o/FileSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fileSearch.o;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
